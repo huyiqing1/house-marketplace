fa3771fefc15072dd083487fe0216858b605451b
huyiqing1 <huyiqing1@gmail.com>
2022-07-07 12:49:55 -0400

    added user sign in function

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,17 @@
+import { useEffect, useState } from "react";
+import { getAuth } from "firebase/auth";
+
 const Profile = () => {
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        const auth = getAuth();
+        setUser(auth.currentUser);
+    }, [])
+
     return (
         <div className="profile">
-            Profile
+            {user ? <h1>{user.displayName}</h1> : "Not Logged In"}
         </div>
     );
 }
